e5f83000667a7da5865ccb3501895d8c2a1056da
Julien Schmidt <github@julienschmidt.com>
2015-09-27 01:45:04 +0200

    Update Mutex implementation

--- mutex.go
+++ mutex.go
@@ -1,3 +1,7 @@
+// Copyright 2015 Julien Schmidt. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file.
+
 package spinlock
 
 import (
@@ -10,28 +14,35 @@ const (
 	mutexLocked   = 1
 )
 
-type Mutex int32
+type Mutex struct {
+	state int32
+}
 
 // Lock locks m.
 // If the lock is already in use, the calling goroutine repetitively tries to
 // acquire the the mutex until it is available (busy waiting).
 func (m *Mutex) Lock() {
-	for !atomic.CompareAndSwapInt32(&m, mutexUnlocked, mutexLocked) {
-	}
-}
-
-// LockYield locks m.
-// Behaves like Lock, but yields the processor, allowing other goroutines to
-// run, each time it fails to acquire the mutex.
-func (m *Mutex) LockYield() {
-	for !atomic.CompareAndSwapInt32(&m, mutexUnlocked, mutexLocked) {
+	for !atomic.CompareAndSwapInt32(&m.state, mutexUnlocked, mutexLocked) {
 		runtime.Gosched()
 	}
 }
 
+// TryLock tries to lock m.
+// If the lock is already in use, the lock is not acquired and false is
+// returned.
+func (m *Mutex) TryLock() bool {
+	return atomic.CompareAndSwapInt32(&m.state, mutexUnlocked, mutexLocked)
+}
+
+// Unlock unlocks m.
+// It is a run-time error if m is not locked on entry to Unlock.
+//
+// A locked Mutex is not associated with a particular goroutine.
+// It is allowed for one goroutine to lock a Mutex and then
+// arrange for another goroutine to unlock it.
 func (m *Mutex) Unlock() {
-	state := atomic.AddInt32(&m, -mutexLocked)
-	if (state + mutexLocked) != mutexUnlocked {
+	state := atomic.AddInt32(&m.state, -mutexLocked)
+	if state != mutexUnlocked {
 		panic("spinlock: unlock of unlocked mutex")
 	}
 }
